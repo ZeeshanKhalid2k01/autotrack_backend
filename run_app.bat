4facc3e1d402ee02bb22ac3bdd7a8270a61d8b3c
Zeeshan Khalid <zeeshankhalid.2k01@gmail.com>
2024-07-04 15:16:15 +0500

    Create run_app.bat

---
+++ run_app.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM Activate conda environment
+call C:\path\to\your\conda.bat activate your_env_name  REM Update this to the actual path of your conda.bat and environment name
+
+REM Run the GUI script
+python gui.py
